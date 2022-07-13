a0ff4e324fafa06b1a13ed2270e2520e2cab3091
lattefor <latteforever@gmail.com>
2022-07-12 21:12:46 -0700

    missing updated eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css/style.css');
     eleventyConfig.addPassthroughCopy('src/images');
     eleventyConfig.addPassthroughCopy('src/js');
+    eleventyConfig.addPassthroughCopy('src/admin');
+    
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
